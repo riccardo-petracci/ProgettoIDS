bc4f02c6da50cee9e45eb6b3737cb346c954db6f
Luca <lucabrandimarti.00@gmail.com>
2019-07-06 09:27:55 +0200

    removing old files

--- bacheca-comunale/app.js
+++ bacheca-comunale/app.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const path = require('path')
+
 const app = express()
 const PORT = process.env.PORT || 8080;
 
-
+app.use('/',require('./routes/homepage'))
 
 
 app.listen(PORT,()=> console.log(`server listening on port ${PORT}`));
