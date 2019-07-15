6b15fae7a3bb6dfbc88f3c09671da0ef1de5daeb
riccardo-petracci <38490032+riccardo-petracci@users.noreply.github.com>
2019-07-15 14:50:02 +0200

    Add files via upload

---
+++ bacheca-comunale/controller/cittadino.js
@@ -0,0 +1,60 @@
+class gestoreCittadino {
+  
+    
+    constructor(formEvent,gestoreEvento) 
+    {
+      this.formEvent = formEvent
+      this.gestoreEvento = gestoreEvento
+    }
+   
+    getCompiledForm()
+    {
+
+    }
+    
+    getEmptyForm()
+    {
+        
+    }
+
+    notifyEventCreation()
+    {
+        
+    }
+
+    newEvent(event)
+    {
+        
+    }
+  }
+
+
+  class gestoreEventi {
+  
+    
+    constructor(formEvent,gestoreNotifiche) 
+    {
+        this.formEvent = formEvent
+        this.gestoreNotifiche = gestoreNotifiche
+    }
+   
+    getEventoApprovare()
+    {
+
+    }
+    
+    inserisciEvento()
+    {
+        
+    }
+
+    notificaUfficioSindacale()
+    {
+        
+    }
+
+    notificaCittadino()
+    {
+        
+    }
+  }
